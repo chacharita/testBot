e35f57ecae6161bb07099db41e81e1d255c08b09
chacharita <chaz9008@gmail.com>
2017-06-21 13:17:58 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,8 +1,15 @@
 ?php
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+$access_token = 'LNVFUuQGohgqx9acOtpSSfaf7V+scYu39GeSMnL4iVnbTNPNhzMuIqYpbRw88IXMm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDsOkcI9sYomzG2oRYNpNLWwMpEe8VNIywbsFm3xDahWsQdB04t89/1O/w1cDnyilFU=';
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('<to>', $textMessageBuilder);
+$url = 'https://api.line.me/v1/oauth/verify';
 
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
