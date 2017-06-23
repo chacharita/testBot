e1592718c433dd87ea4db5cc759e04fad9c7abde
chacharita <chaz9008@gmail.com>
2017-06-23 11:06:35 +0700

    Update PUSH BOT.php

--- PUSH BOT.php
+++ PUSH BOT.php
@@ -1,7 +1,7 @@
 <?php
  
 $strAccessToken = "EOFJCU+NGmGZ4ca8OVjNKzz6WkNdqcrWKS2KK11Fx1g1Ti0BNdDPa019isWZHK47m31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDuZctrN5jBIO04MagDmuy0poH2Hd4aQXAO7TKs5UM/ofAdB04t89/1O/w1cDnyilFU=";
- 
+//เข้ารหัสที่เป็นสตริง = strUrl
 $strUrl = "https://api.line.me/v2/bot/message/push";
  
 $arrHeader = array();
@@ -9,7 +9,7 @@ $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
 $arrPostData = array();
-$arrPostData['to'] = "@wov8417b";
+$arrPostData['to'] = "ch_rita";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
  
@@ -28,7 +28,7 @@ curl_close ($ch);
 //-------------------------------------------------------------------------------------
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<EOFJCU+NGmGZ4ca8OVjNKzz6WkNdqcrWKS2KK11Fx1g1Ti0BNdDPa019isWZHK47m31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDuZctrN5jBIO04MagDmuy0poH2Hd4aQXAO7TKs5UM/ofAdB04t89/1O/w1cDnyilFU=>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<	
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' =>'< 
 5cc92d0cde1b2781c60f291c953befa1>']);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
