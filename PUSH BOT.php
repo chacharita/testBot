a7a7fbd7aabeb77c5f7825945a45b8183a8223de
chacharita <chaz9008@gmail.com>
2017-06-23 10:52:29 +0700

    Update PUSH BOT.php

--- PUSH BOT.php
+++ PUSH BOT.php
@@ -25,4 +25,14 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
  
+//-------------------------------------------------------------------------------------
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<EOFJCU+NGmGZ4ca8OVjNKzz6WkNdqcrWKS2KK11Fx1g1Ti0BNdDPa019isWZHK47m31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDuZctrN5jBIO04MagDmuy0poH2Hd4aQXAO7TKs5UM/ofAdB04t89/1O/w1cDnyilFU=>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<	
+5cc92d0cde1b2781c60f291c953befa1>']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->pushMessage('<ch_rita>', $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
