4d1bcaa54530a5306f239ada5ea6dafc9255446f
chacharita <chaz9008@gmail.com>
2017-06-23 11:13:39 +0700

    Update PUSH BOT.php

--- PUSH BOT.php
+++ PUSH BOT.php
@@ -9,7 +9,7 @@ $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
 $arrPostData = array();
-$arrPostData['to'] = "ch_rita";
+$arrPostData['to'] = " ";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
  
