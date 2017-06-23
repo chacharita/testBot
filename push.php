5c8deffe4077f8592018bf2a6c4f34317c64e254
chacharita <chaz9008@gmail.com>
2017-06-23 12:22:05 +0700

    Create push.php

---
+++ push.php
@@ -0,0 +1,9 @@
+<?php
+httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('EOFJCU+NGmGZ4ca8OVjNKzz6WkNdqcrWKS2KK11Fx1g1Ti0BNdDPa019isWZHK47m31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDuZctrN5jBIO04MagDmuy0poH2Hd4aQXAO7TKs5UM/ofAdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '	
+5cc92d0cde1b2781c60f291c953befa1']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->pushMessage('ch.rita', $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
