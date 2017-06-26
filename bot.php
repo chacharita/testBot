dceebf74eb82c6d65079ae629ba2b7723de75bfe
chacharita <chaz9008@gmail.com>
2017-06-26 21:16:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,7 +5,7 @@ $mids = array(0=>'Uf9273c1fa1ec2ff4c3fb01f81d86556b');
 foreach($mids as $key => $mid){        
         $messages = [
             "type" => "text",
-            "text" => $_POST;
+            "text" => $_POST["text"];
         ];
  
         $post_data = [
