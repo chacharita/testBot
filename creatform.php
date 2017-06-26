d1977e29a39b05b6f9a2d5b21296c9fe7836edac
chacharita <chaz9008@gmail.com>
2017-06-26 21:09:50 +0700

    Create creatform.php

---
+++ creatform.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+    <div>
+   <h1 style="text-align:left;background-color: #339933">LINE@PUSH_MESSAGE</h1>
+    <form action="bot.php" method="POST">
+    
+        
+      INPUT TEXT :<br>
+      <TEXTAREA rows="10" cols="100" name="txet"> 
+</TEXTAREA>                                             
+         <br>
+        <input type="submit" value="SUBMIT" >         
+    </form>
+    </div>
+</body>
+</html>
