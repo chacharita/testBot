7f71122c78dd663b77ca6ec45006874d7829afd7
chacharita <chaz9008@gmail.com>
2017-06-26 14:45:14 +0700

    Update push.php

--- push.php
+++ push.php
@@ -9,7 +9,7 @@ $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
 $arrPostData = array();
-$arrPostData['to'] = "USER_ID";
+$arrPostData['to'] = "U7de80d0a2ceea863e831375badd2eb55";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
  
