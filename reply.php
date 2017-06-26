399577a091ebff0fee4a7ebe670e9a47b37aafee
chacharita <chaz9008@gmail.com>
2017-06-26 21:53:23 +0700

    Update reply.php

--- reply.php
+++ reply.php
@@ -15,7 +15,7 @@ if($arrJson['events'][0]['message']['text'] == "Hi" or "hi"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "UserID".$arrJson['events'][0]['source']['userId'];
+  $arrPostData['messages'][0]['text'] = "UserID :: ".$arrJson['events'][0]['source']['userId'];
 // }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
 //   $arrPostData = array();
 //   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
