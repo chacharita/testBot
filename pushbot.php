67cf3f3378c2add04188499d114d3b3524096d5b
chacharita <chaz9008@gmail.com>
2017-06-27 14:44:49 +0700

    Update pushbot.php

--- pushbot.php
+++ pushbot.php
@@ -2,7 +2,7 @@
        
 $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=";
 
-$mids = array(0=>'Uf9273c1fa1ec2ff4c3fb01f81d86556b'); 
+$mids = array(0=>'U8e595fe987f94b2efd9db19c6039a1e1'); 
 
 foreach($mids as $key => $mid){        
         $messages = [
