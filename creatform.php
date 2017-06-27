bfcbb4e8cade0e17c0c5700675e980f6cd6fa729
chacharita <chaz9008@gmail.com>
2017-06-27 17:05:29 +0700

    Update creatform.php

--- creatform.php
+++ creatform.php
@@ -6,15 +6,16 @@
     <h1 style="text-align:left;background-color: #339933">LINE@PUSH_MESSAGE</h1>
    
     <form action="pushbot.php" method="POST">
-     INPUT MESSAGE <br>
+     INPUT MESSAGE 
             
-    <input type="text" name="inputtext" size="50" width="1200">
+    <input type="text" name="inputtext" size="50" width="1200"><br>
                               
-       <input type="submit" value="SUBMIT" >   
+       
         <label for="inputimage" class="col-lg-2 control-label">Photo URL</label>
-         <input type="text" class="form-control" id="inputimage" name="inputimage" placeholder="Photo URL">
+         <input type="text" class="form-control" id="inputimage" name="inputimage" placeholder="Photo URL" size="60"><br>
          <input type="submit" value="SUBMIT" > 
     </form>
     </div>
 </body>
 </html>
+
