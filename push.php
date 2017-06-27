8f01c5982cc7e6e8a87666476c8dc00d0d15b25e
chacharita <chaz9008@gmail.com>
2017-06-27 10:45:56 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,9 +1,29 @@
 <?php
+$strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=";
+
+//$mids = array(0=>'Uf9273c1fa1ec2ff4c3fb01f81d86556b');  
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '5cc92d0cde1b2781c60f291c953befa1']);
+$arrPostData = array();
+$arrPostData['to'] = "Uf9273c1fa1ec2ff4c3fb01f81d86556b";
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('U8e595fe987f94b2efd9db19c6039a1e1', $textMessageBuilder);
+if(curl_error($ch)) { echo 'error:'. curl_error($ch); } 
+else { $result_ = json_decode($result, true); 
+echo "status : ".$result_['status']; echo "message : ". $result_['message']; }
 
-echo $response->getHTTPStatus() . ' 55555' . $response->getRawBody();
+curl_close ($ch);
