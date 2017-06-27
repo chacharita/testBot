789d552c99a31a9b62d56d86b324726085ccd8d4
chacharita <chaz9008@gmail.com>
2017-06-27 15:51:03 +0700

    Update creatform.php

--- creatform.php
+++ creatform.php
@@ -2,10 +2,12 @@
 <html>
 <body>
     <div>
+    <body bgcolor = "#99FF99"> ... </body>
     <h1 style="text-align:left;background-color: #339933">LINE@PUSH_MESSAGE</h1>
     <form action="pushbot.php" method="POST">
      INPUT MESSAGE <br>
      <input type="text" name="inputtext"><br>
+       <input type="image" name="inputimage"><br>
                                                     
        <input type="submit" value="SUBMIT" >      
     </form>
