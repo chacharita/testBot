68d03c9603ee34c704357780c51eccfaf3d254d6
chacharita <chaz9008@gmail.com>
2017-06-27 11:50:48 +0700

    Update pushbot.php

--- pushbot.php
+++ pushbot.php
@@ -3,11 +3,11 @@
 $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=";
 
 $mids = array(0=>'Uf9273c1fa1ec2ff4c3fb01f81d86556b'); 
-
+$text = $_POST;
 foreach($mids as $key => $mid){        
         $messages = '[
             "type" => "text"
-            "text" => "Welcome To DX "
+            "text" => $text;
         ];
  
         $post_data = [
