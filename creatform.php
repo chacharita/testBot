ba87962e233b24424c975b0fa6896d974b011df6
chacharita <chaz9008@gmail.com>
2017-06-27 14:24:32 +0700

    Update creatform.php

--- creatform.php
+++ creatform.php
@@ -1,12 +1,13 @@
+<!DOCTYPE html>
 <html>
 <body>
     <div>
-   <h1 style="text-align:left;background-color: #339933">LINE@PUSH_MESSAGE</h1>
-    <form action="pushbot.php" method="REQUEST">     
-      INPUT TEXT :<br>
-      <TEXTAREA rows="10" cols="100" name="txet"></TEXTAREA>                                             
-         <br>
-        <input type="submit" value="SUBMIT" >         
+    <h1 style="text-align:left;background-color: #339933">LINE@PUSH_MESSAGE</h1>
+    <form action="pushbot.php" method="POST">
+     INPUT MESSAGE <br>
+     <input type="text" name="input text"><br>
+                                                    
+       <input type="submit" value="SUBMIT" >      
     </form>
     </div>
 </body>
