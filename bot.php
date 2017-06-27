13f7d3be09f6d9d4e2aa271a5440396b539ddd30
chacharita <chaz9008@gmail.com>
2017-06-27 10:04:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,7 +7,7 @@ $mids = array(0=>'Uf9273c1fa1ec2ff4c3fb01f81d86556b');
 foreach($mids as $key => $mid){        
         $messages = [
             "type" => "text"
-            "text" => ' '
+            "text" => 'Welcome To DX '
         ];
  
         $post_data = [
