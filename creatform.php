5e053e619b1021570ce1a1476829f55795e86f1e
chacharita <chaz9008@gmail.com>
2017-06-28 18:14:21 +0700

    Update creatform.php

--- creatform.php
+++ creatform.php
@@ -6,20 +6,18 @@
     <h1 style="text-align:left;background-color: #99FFFF">LINE@PUSH_MESSAGE</h1>
    
     <form action="pushbot.php" method="POST">
-     INPUT MESSAGE 
-            
-    <input type="text" :center ; name="inputtext" size="50" width="1200"><br>
-        
-                                       
-       
-        <label for="inputimage" class="col-lg-2 control-label">Photo URL</label>
-         <input type="text" class="form-control" id="inputimage" name="inputimage" placeholder="Photo URL" size="60"><br>
-        <SELECT> 
+    <SELECT>
          <OPTION SELECTED>---เลือกMember---
          <OPTION VALUE=U7de80d0a2ceea863e831375badd2eb55>ffon 
          <OPTION VALUE=U8e595fe987f94b2efd9db19c6039a1e1>OIL
          <OPTION VALUE=U16c42e452723cf9c2682d7bf0001b0d8>P'eam  
-         </SELECT>
+      </SELECT>
+             
+        INPUT MESSAGE <input type="text" :center ; name="inputtext" size="50" width="1200"><br>
+        
+        <label for="inputimage" class="col-lg-2 control-label">Photo URL</label>
+         <input type="text" class="form-control" id="inputimage" name="inputimage" placeholder="Photo URL" size="60"><br>
+        
          <input type="submit" value="SUBMIT" > 
     </form>
     </div>
