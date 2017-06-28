ba142929207adc9b0933e7b1eeb4e7d622ce12c1
chacharita <chaz9008@gmail.com>
2017-06-28 11:43:30 +0700

    Update pushbot.php

--- pushbot.php
+++ pushbot.php
@@ -4,7 +4,7 @@ $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQG
 $text= $_POST['inputtext'];
 $image = $_POST['inputimage'];
 $mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55','U8e595fe987f94b2efd9db19c6039a1e1','U16c42e452723cf9c2682d7bf0001b0d8');
-
+var_dump($mids);
 foreach($mids as $key => $mid){        
         $messages = [
             "type" => "text",
