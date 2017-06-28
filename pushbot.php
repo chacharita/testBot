1ce16c071837d2fec877f737f82e61423ac4a619
chacharita <chaz9008@gmail.com>
2017-06-28 12:18:49 +0700

    Update pushbot.php

--- pushbot.php
+++ pushbot.php
@@ -3,22 +3,20 @@
 $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=";
 var_dump($strAccessToken);
 $text= $_POST['inputtext'];
-var_dump($text);
-$image = $_POST['inputimage'];
-$mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55','U8e595fe987f94b2efd9db19c6039a1e1','U16c42e452723cf9c2682d7bf0001b0d8');
-var_dump($mids);
-foreach($mids as $key => $mid){        
+
+//$image = $_POST['inputimage'];
+//$mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55','U8e595fe987f94b2efd9db19c6039a1e1','U16c42e452723cf9c2682d7bf0001b0d8');
+
+//var_dump($mids);
+foreach($messages as $mid =>$mids){
+       
+       foreach($mids as $key => $mid){        
         $messages = [
             "type" => "text",
             "text" => $text 
            
          ];
-       
-    
-// //                "type" =>"image",
-// //               "originalContentUrl"=> "https://example.com/original.jpg",
-// //                "previewImageUrl"=> "https://example.com/preview.jpg"
-       
+      
        
       
        
@@ -46,6 +44,7 @@ foreach($mids as $key => $mid){
         $result = curl_exec($ch);
         curl_close($ch);
        
+}
 }
 
 
