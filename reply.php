dea033d49aad516f9c6a0d2a3c0268fe8d01cf04
chacharita <chaz9008@gmail.com>
2017-06-28 12:32:47 +0700

    Update reply.php

--- reply.php
+++ reply.php
@@ -5,7 +5,6 @@ $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQG
 $content = file_get_contents('php://input');
 var_dump($content);
 $arrJson = json_decode($content, true);
- 
 $strUrl = "https://api.line.me/v2/bot/message/reply";
 var_dump($strUrl); 
 $arrHeader = array();
