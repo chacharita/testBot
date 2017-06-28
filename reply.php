24fbe3018f308aea4ac346ffdf90780c5c13bee3
chacharita <chaz9008@gmail.com>
2017-06-28 09:59:18 +0700

    Update reply.php

--- reply.php
+++ reply.php
@@ -16,16 +16,7 @@ if($arrJson['events'][0]['message']['text'] == "Hi" or "hi"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "UserID :: ".$arrJson['events'][0]['source']['userId'];
-// }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
-//   $arrPostData = array();
-//   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-//   $arrPostData['messages'][0]['type'] = "text";
-//   $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-// }else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
-//   $arrPostData = array();
-//   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-//   $arrPostData['messages'][0]['type'] = "text";
-//   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
