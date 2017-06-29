f28c9924ba1183721376ab90ac309a41d7ab4b5c
chacharita <chaz9008@gmail.com>
2017-06-29 10:41:54 +0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+    <div>
+    <center>
+   <h1 style="text-align:center;">PUSH MESSAGE</h1>
+    <form action="pushbot.php" method="POST" >  
+      <br><br>
+      <TEXTAREA  name="txet"rows="10" cols="80"> </TEXTAREA>                                             
+         <br><br><br>
+         <SELECT name="[name]"> 
+         <OPTION SELECTED>---selet member---
+         <OPTION VALUE=U7de80d0a2ceea863e831375badd2eb55>ffon
+         <OPTION VALUE=U8e595fe987f94b2efd9db19c6039a1e1>Oil
+         </SELECT>
+        <input type="submit" value="SUBMIT" >         
+    </form>
+    </div>
+</body>
+</html>
