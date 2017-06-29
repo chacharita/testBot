c629b153c73c5a541560e8b409f4ce5e2a789f36
chacharita <chaz9008@gmail.com>
2017-06-29 12:12:16 +0700

    Update creatform.php

--- creatform.php
+++ creatform.php
@@ -8,15 +8,14 @@
     <form action="pushbot.php" method="POST">
     <SELECT>
          <OPTION name="[name]">--- เลือก Member ---
-         <OPTION mids=U7de80d0a2ceea863e831375badd2eb55>ffon 
-         <OPTION mids=U8e595fe987f94b2efd9db19c6039a1e1>OIL
-         <OPTION mids=U16c42e452723cf9c2682d7bf0001b0d8>P'eam  
+         <OPTION VALUE=U7de80d0a2ceea863e831375badd2eb55>ffon 
+         <OPTION VALUE=U8e595fe987f94b2efd9db19c6039a1e1>OIL
+         <OPTION VALUE=U16c42e452723cf9c2682d7bf0001b0d8>P'eam  
       </SELECT>
         <br>     
         INPUT MESSAGE <input type="text" :center ; name="inputtext" size="50" width="1200"><br>
         
-        <label for="inputimage" class="col-lg-2 control-label">Photo URL</label>
-         <input type="text" class="form-control" id="inputimage" name="inputimage" placeholder="Photo URL" size="60"><br>
+       
         
          <input type="submit" value="SUBMIT" > 
     </form>
