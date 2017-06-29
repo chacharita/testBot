38a251bd55c6a69792ebc56e3b179040c23df064
chacharita <chaz9008@gmail.com>
2017-06-29 13:45:57 +0700

    Update form.php

--- form.php
+++ form.php
@@ -7,8 +7,8 @@
       <br><br>
       <TEXTAREA  type ="text" name="txet"rows="10" cols="80"> </TEXTAREA>                                             
          <br><br><br>
-          <input type="checkbox" name="vehicle" value="U7de80d0a2ceea863e831375badd2eb55">FON<br>
-          <input type="checkbox" name="vehicle" value="U8e595fe987f94b2efd9db19c6039a1e1">CHUPPY<br>
+<!--           <input type="checkbox" value="">TokenFON<br> -->
+          <input type="checkbox" value="xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=">TokenOIL<br>
         <input type="submit" value="SUBMIT" >         
     </form>
     </div>
