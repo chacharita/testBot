771123b74ca594de066ff835f42056fbe7c6365c
chacharita <chaz9008@gmail.com>
2017-06-29 11:11:35 +0700

    Update pushbot.php

--- pushbot.php
+++ pushbot.php
@@ -1,14 +1,14 @@
 <?php
        
 $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=";
-var_dump($strAccessToken);
+//var_dump($strAccessToken);
 $text= $_POST['inputtext'];
 
 //$image = $_POST['inputimage'];
-//$mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55','U8e595fe987f94b2efd9db19c6039a1e1','U16c42e452723cf9c2682d7bf0001b0d8');
+$mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55','U8e595fe987f94b2efd9db19c6039a1e1','U16c42e452723cf9c2682d7bf0001b0d8');
 
 //var_dump($mids);
-foreach($messages as $mid =>$mids){
+//foreach($messages as $mid =>$mids){
        
        foreach($mids as $key => $mid){        
         $messages = [
@@ -38,7 +38,7 @@ foreach($messages as $mid =>$mids){
         $result = curl_exec($ch);
         curl_close($ch);
        
-}
+//}
 }
 
 
