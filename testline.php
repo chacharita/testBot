98ac07ab11aea32e05f8b0fe3ec16b868d690de8
chacharita <chaz9008@gmail.com>
2017-06-29 17:40:41 +0700

    Update testline.php

--- testline.php
+++ testline.php
@@ -5,6 +5,22 @@
     <title>Push Messages</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        .head-form h1 {
+            padding-top: 30px;
+            padding-bottom: 50px;
+        }
+        #myModal {
+            margin-top: 100px;
+        }
+        .button-sc .button {
+            margin-bottom: 30px;
+            margin-top: 20px;
+        }
+    </style>
 
 </head>
 <body>
