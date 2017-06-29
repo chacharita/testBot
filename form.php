0eb49d9b765b049ca238e7785dd5e497c45f2a7b
chacharita <chaz9008@gmail.com>
2017-06-29 12:37:23 +0700

    Update form.php

--- form.php
+++ form.php
@@ -7,11 +7,8 @@
       <br><br>
       <TEXTAREA  name="txet"rows="10" cols="80"> </TEXTAREA>                                             
          <br><br><br>
-         <SELECT name="[name]"> 
-         <OPTION SELECTED>---selet member---
-         <OPTION VALUE=U7de80d0a2ceea863e831375badd2eb55>ffon
-         <OPTION VALUE=U8e595fe987f94b2efd9db19c6039a1e1>Oil
-         </SELECT>
+          <input type="checkbox" name="vehicle" value="U7de80d0a2ceea863e831375badd2eb55">FON<br>
+          <input type="checkbox" name="vehicle" value="U8e595fe987f94b2efd9db19c6039a1e1" checked>CHUPPY<br>
         <input type="submit" value="SUBMIT" >         
     </form>
     </div>
