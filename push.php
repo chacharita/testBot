6c95f076ec50daf695efc91fba33cb74df4f590d
chacharita <chaz9008@gmail.com>
2017-06-29 12:42:24 +0700

    Update push.php

--- push.php
+++ push.php
@@ -3,9 +3,9 @@
 $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=";
 //var_dump($strAccessToken);
 $text= $_POST['text'];
-$VALUE = $_POST['name'];
+$checkbox = $_POST['vehicle'];
 //$image = $_POST['inputimage'];
-$mids = array($VALUE);
+$mids = array($checkbox);
 
 //var_dump($mids);
 //foreach($messages as $mid =>$mids){
