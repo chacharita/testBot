616f933ed76a821cb7d66eb4ed6d9af1d4cf80d2
chacharita <chaz9008@gmail.com>
2017-06-29 11:46:30 +0700

    Update pushbot.php

--- pushbot.php
+++ pushbot.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <div>
-    <center>
-    <h1 style="text-align>PUSH_MESSAGE</h1>
-   <center>
-    <form action="pushbot.php" method="POST">
-    <SELECT>
-         <OPTION name="[name]">--- เลือก Member ---
-         <OPTION VALUE=U7de80d0a2ceea863e831375badd2eb55>ffon 
-         <OPTION VALUE=U8e595fe987f94b2efd9db19c6039a1e1>OIL
-         <OPTION VALUE=U16c42e452723cf9c2682d7bf0001b0d8>P'eam  
-      </SELECT>
-        <br>     
-        INPUT MESSAGE <input type="text" :center ; name="inputtext" size="50" width="1200"><br>
-        
-        <label for="inputimage" class="col-lg-2 control-label">Photo URL</label>
-         <input type="text" class="form-control" id="inputimage" name="inputimage" placeholder="Photo URL" size="60"><br>
-        
-         <input type="submit" value="SUBMIT" > 
-    </form>
-    </div>
-</body>
-</html>
 
 <?php
        
