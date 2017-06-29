b6fb03e843107aa860dbbf3059c73218f6f9f41b
chacharita <chaz9008@gmail.com>
2017-06-29 14:40:27 +0700

    Update form.php

--- form.php
+++ form.php
@@ -8,7 +8,7 @@
       <TEXTAREA  type ="text" name="txet"rows="10" cols="80"> </TEXTAREA>                                             
          <br><br><br>
 <!--           <input type="checkbox" value="">TokenFON<br> -->
-          <input type="checkbox" value="xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=">TokenOIL<br>
+         select Token <input type="checkbox" value="xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=">TokenOIL<br>
         <input type="submit" value="SUBMIT" >         
     </form>
     </div>
