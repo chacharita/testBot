bcaa1d3fb564be7aba1dd85ce890fe9425d9cf33
chacharita <chaz9008@gmail.com>
2017-06-29 12:40:11 +0700

    Update form.php

--- form.php
+++ form.php
@@ -5,7 +5,7 @@
    <h1 style="text-align:center;">PUSH MESSAGE</h1>
     <form action="push.php" method="POST" >  
       <br><br>
-      <TEXTAREA  name="txet"rows="10" cols="80"> </TEXTAREA>                                             
+      <TEXTAREA  type ="text" name="txet"rows="10" cols="80"> </TEXTAREA>                                             
          <br><br><br>
           <input type="checkbox" name="vehicle" value="U7de80d0a2ceea863e831375badd2eb55">FON<br>
           <input type="checkbox" name="vehicle" value="U8e595fe987f94b2efd9db19c6039a1e1">CHUPPY<br>
