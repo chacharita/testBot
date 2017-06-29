082bee5f1ed6cca0bf1db0d90bc55acda6bf3506
chacharita <chaz9008@gmail.com>
2017-06-29 10:18:04 +0700

    Update creatform.php

--- creatform.php
+++ creatform.php
@@ -3,7 +3,7 @@
 <body>
     <div>
     
-    <h1 style="text-align:left;background-color: #99FFFF">LINE@PUSH_MESSAGE</h1>
+    <h1 style="text-align:center;background-color: #99FFFF">LINE@PUSH_MESSAGE</h1>
    <center>
     <form action="pushbot.php" method="POST">
     <SELECT>
