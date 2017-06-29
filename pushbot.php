64618849581a7106b0daec99c26935b0b7a4a616
chacharita <chaz9008@gmail.com>
2017-06-29 11:49:31 +0700

    Update pushbot.php

--- pushbot.php
+++ pushbot.php
@@ -1,9 +1,30 @@
-
+<html>
+<body>
+    <div>
+    <center>
+   <h1 style="text-align:center;">PUSH MESSAGE</h1>
+   
+   <form action="pushbot.php" method="POST" >  
+      <br><br>
+      <TEXTAREA  name="txet"rows="10" cols="80"> </TEXTAREA>                                             
+         <br><br><br>
+         <SELECT name="[name]"> 
+         <OPTION SELECTED>---selet member---
+         <OPTION VALUE=U7de80d0a2ceea863e831375badd2eb55>ffon
+         <OPTION VALUE=U8e595fe987f94b2efd9db19c6039a1e1>Oil
+         </SELECT>
+        <input type="submit" value="SUBMIT" >
+      
+     
+    </form>
+    </div>
+</body>
+</html>
 <?php
        
 $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=";
 
-$text= $_POST['inputtext'];
+$text= $_POST['text'];
 $user = $_POST['name'];
 
 $mids = array($user);
