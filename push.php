2cce8c7d9dcc77382f987adb8c3aef59ce6c66c8
chacharita <chaz9008@gmail.com>
2017-06-29 10:40:40 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,29 +1,45 @@
 <?php
+       
 $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=";
+var_dump($strAccessToken);
+$text= $_POST['text'];
+$user = $_POST['name'];
+//$image = $_POST['inputimage'];
+//$mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55','U8e595fe987f94b2efd9db19c6039a1e1','U16c42e452723cf9c2682d7bf0001b0d8');
 
-//$mids = array(0=>'Uf9273c1fa1ec2ff4c3fb01f81d86556b');  
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+//var_dump($mids);
+//foreach($messages as $mid =>$mids){
+       
+       foreach($mids as $key => $mid){        
+        $messages = [
+            "type" => "text",
+            "text" => $text 
+           
+         ];
+    
+      $post_data = [
+            "to" => $mid,
+            "messages" => [$messages]
+        ];
+        
+   
+        $header = array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $strAccessToken
+        );
+        $url = 'https://api.line.me/v2/bot/message/push';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
  
-$arrPostData = array();
-$arrPostData['to'] = "Uf9273c1fa1ec2ff4c3fb01f81d86556b";
-$arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
- 
- 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
+        $result = curl_exec($ch);
+        curl_close($ch);
+       
+}
+}
 
-if(curl_error($ch)) { echo 'error:'. curl_error($ch); } 
-else { $result_ = json_decode($result, true); 
-echo "status : ".$result_['status']; echo "message : ". $result_['message']; }
 
-curl_close ($ch);
+?>
