29d53f1eb8bb46848de3035ad1a6bf535fe74d7e
chacharita <chaz9008@gmail.com>
2017-06-29 12:22:34 +0700

    Update push.php

--- push.php
+++ push.php
@@ -5,7 +5,7 @@ $strAccessToken = "xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQG
 $text= $_POST['text'];
 $VALUE = $_POST['name'];
 //$image = $_POST['inputimage'];
-$mids = array(0=>'U8e595fe987f94b2efd9db19c6039a1e1');
+$mids = array($VALUE);
 
 //var_dump($mids);
 //foreach($messages as $mid =>$mids){
