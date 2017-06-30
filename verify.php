763a402f37796e996ab43d5b9234a87415fbc6fd
chacharita <chaz9008@gmail.com>
2017-06-30 10:20:36 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -9,6 +9,8 @@ $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
