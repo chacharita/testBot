abdba2bec86daa8983edec49248eb05f37cdac6d
chacharita <chaz9008@gmail.com>
2017-06-30 10:18:34 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,7 @@
 <?php
+
+$proxy = 'http://fixie:bBt21X0wwYroR2Z@velodrome.usefixie.com:80';
+$proxyauth = 'http://fixie:bBt21X0wwYroR2Z@velodrome.usefixie.com:80';
 $access_token = 'xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
