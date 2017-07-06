c151090fced7c6ada402a35df032b211f478bc3e
chacharita <chaz9008@gmail.com>
2017-07-06 17:31:31 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <div>
-    
-   <h1>REGISTER</h1>
-   
-    <form method="GET"> 
-
-      Date:<input type="text" name="date_time"><br>  
-      st_y: <input type="text" name="st_y"><br>
-      st_x : <input type="text" name="st_x"><br>
-      en_y: <input type="text" name="en_y"><br>  
-      en_x: <input type="text" name="en_x"><br> 
-      toll: <input type="text" name="toll"><br><br>    
-      <input type="submit" value="submit">         
-    </form>
-    </div>
-</body>
-</html>
 
 
 <?php
