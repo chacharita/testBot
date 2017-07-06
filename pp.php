1fba4de54c3fcebd5001492bf2f43c24d79dce44
chacharita <chaz9008@gmail.com>
2017-07-06 15:30:19 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -1,34 +1,30 @@
 <?php
    
-    $urlProtocol = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-04-26T08:00:00&st_y=13.
-    8036917&st_x=100.5542416&en_y=13.8755595&en_x=100.6191486&toll=1 ";     
-    
-    //$request = "date_time=Date and Time[yyyy-mm-ddThh:mm:ss]".$st_y="Latitude start point".
-    //$st_x="Longitude of start point".$en_y="Latitude of end point".$en_x="Longitude of end point".
-   // $toll="toll usage (0: not use, 1: use)";   
+    $url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?";     
+    $strAccessToken = true;
+   // $post_data =[
+    //];
+        
+       
+   // $isRequestHeader = false;
    
-    $isRequestHeader = false;
+    $header = array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $strAccessToken
+            );
+   
     
-    
-    $exHeaderInfoArr   = array();
-    $exHeaderInfoArr[] = "Content-Type: application/json";
-    $exHeaderInfoArr[] = "Authorization: Bearer".$urlProtocol;
-   // $url = 'https://api/message/request' ;
-    
-    $ch = curl_init($urlProtocol);
-   // $ch = curl_init("$urlProtocol");
-    curl_setopt($ch, CURLOPT_URL, $urlProtocol);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
-    curl_setopt($ch, CURLOPT_HEADER, (($isRequestHeader) ? 1 : 0));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    if( is_array($exHeaderInfo) && !empty($exHeaderInfo) )
-    {
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $exHeaderInfo);
-    }
-    $response = curl_exec($ch);
-    curl_close($ch);
+    $ch = curl_init($url);
  
+            curl_setopt($ch, CURLOPT_GET, true);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS,($date_time , $st_y, $st_x,$en_y , $toll));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            $result = curl_exec($ch);
+            curl_close($ch);
+       
+   
     echo $response;
 
 
