93f0f70b8b4b2300fc6859cbc8af36dea7a5f4c6
chacharita <chaz9008@gmail.com>
2017-07-06 17:16:13 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -7,7 +7,7 @@
    
     <form method="GET"> 
 
-      Date:<input type="text" name="Date_time"><br>  
+      Date:<input type="text" name="date_time"><br>  
       st_y: <input type="text" name="st_y"><br>
       st_x : <input type="text" name="st_x"><br>
       en_y: <input type="text" name="en_y"><br>  
