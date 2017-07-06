6bdf9e88b493566ccd382603528ca62718716e8c
chacharita <chaz9008@gmail.com>
2017-07-06 16:16:14 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -1,7 +1,7 @@
 <?php
    
     $url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?";     
-    $strAccessToken = true;
+    $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
     
     $post_data =array(  
         $date_time='yyyy-mm-dd' ,
