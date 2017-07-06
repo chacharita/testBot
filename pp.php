aa94ed289f66ba5f99dd532e247b8610d0595c55
chacharita <chaz9008@gmail.com>
2017-07-06 17:44:57 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -2,8 +2,11 @@
 
 <?php
    
-$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting";     
+$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-04-26T08:00:00&st_y=13.8036917&st_x=100.5542416&en_y=13.8755595&en_x=100.6191486&toll=1		
+		
+";     
 $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
+
 
   // var_dump($_GET);
  
@@ -28,7 +31,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
             curl_setopt($ch, CURLOPT_GET, true);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS,($_GET));
+            //curl_setopt($ch, CURLOPT_POSTFIELDS,('?date_time=20&'));
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
             $result = curl_exec($ch);
             curl_close($ch);
