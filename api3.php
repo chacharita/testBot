43bfb13f4f6ca7d679d7f6f61f8f671d7d3aa880
chacharita <chaz9008@gmail.com>
2017-07-07 10:57:18 +0700

    Update api3.php

--- api3.php
+++ api3.php
@@ -23,9 +23,9 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
     $ch = curl_init($url);
  
             curl_setopt($ch, CURLOPT_GET, true);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS,($_GET));
+            curl_setopt($ch, CURLOPT_POSTFIELDS,($_POST));
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
             $result = curl_exec($ch);
             curl_close($ch);
