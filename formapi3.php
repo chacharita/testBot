2e81417cb3a3610fd169815b8cfaedb5714b20eb
chacharita <chaz9008@gmail.com>
2017-07-07 13:51:22 +0700

    Update formapi3.php

--- formapi3.php
+++ formapi3.php
@@ -3,15 +3,15 @@
 <body>
     <div>
     
-   <h1>REGISTER</h1>
-   
+   <h1>Multi-Drop/Multi-Vehicle Routing</h1>
+    
     <form action="api3.php" method="POST"> 
 
-      stops:<input type="text" name="stops"><br> 
-      stops1:<input type="text" name="stops"><br>  
-      vehicles:<input type="text" name="vehicles"><br>
-      vehicles1:<input type="text" name="vehicles"><br>
-      commons:<input type="text" name="commons"><br>
+      stops____: <input type="text" name="stops" value="1,13.8036917,100.5542416,30,08:00:00,17:00:00" size="20"><br> 
+      stops1___: <input type="text" name="stops" value="n2,13.8755595,100.6191486,30,08:00:00,17:00:00" size="19"><br> 
+      vehicles_: <input type="text" name="vehicles" value="1,13.8036917,100.5542416,13.8036917,100.5542416,08:00:00,17:00:00,80" size="19"><br>
+      vehicles1:<input type="text" name="vehicles"  value="2,13.79518,100.61217,13.81042,100.65585,08:00:00,17:00:00,40" size="19"><br>
+      commons__: <input type="text" name="commons" value="2017-06-01,1,0,1" size="19"><br>
        
       <input type="submit" value="submit">         
     </form>
