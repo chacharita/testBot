6de92838c6e09cf9b94ffe6710b79b01188654c2
chacharita <chaz9008@gmail.com>
2017-07-07 11:39:30 +0700

    Update api3.php

--- api3.php
+++ api3.php
@@ -4,21 +4,9 @@ $url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/mvrp";
 $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 
 $stops = $_POST['stops'];
-$stops1 = $_POST['stops'];
 $vehicles = $_POST['vehicles'];
-$vehicles1 = $_POST['vehicles'];
 $commons = $_POST['commons'];
-  // var_dump($_GET);
  
-//    $post_data =array(  
-//         $date_time='yyyy-mm-dd' ,
-//         $st_y = '08:00:00',
-//         $st_x= '08:00:00',     
-//         $en_y= '17:00:00' ,      
-//         $en_x= '17:00:00'  ,    
-//         $toll= '[0 or 1]'
-//     ); 
-       
     
     $header = array(
             'Content-Type:application/x-www-form-urlencoded',
@@ -35,8 +23,6 @@ $commons = $_POST['commons'];
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
             $result = curl_exec($ch);
             curl_close($ch);
-       
-   
-    //echo "response";
+    
     var_dump($result);
 ?>
