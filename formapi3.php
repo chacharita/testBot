5d4c160753bbb0338808b316f6eadcc0633c4297
chacharita <chaz9008@gmail.com>
2017-07-07 10:31:47 +0700

    Create formapi3.php

---
+++ formapi3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div>
+    
+   <h1>REGISTER</h1>
+   
+    <form action="api3.php" method="GET"> 
+
+      stops:<input type="text" name="stops"><br>  
+      vehicles: <input type="text" name="vehicles"><br>
+      commons: <input type="text" name="commons"><br>
+       
+      <input type="submit" value="submit">         
+    </form>
+    </div>
+</body>
+</html>
