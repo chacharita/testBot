ed13c45ce59124abd31dcf8c1861221b6447bda5
chacharita <chaz9008@gmail.com>
2017-07-07 11:30:58 +0700

    Update formapi3.php

--- formapi3.php
+++ formapi3.php
@@ -8,7 +8,9 @@
     <form action="api3.php" method="POST"> 
 
       stops:<input type="text" name="stops"><br>  
+      stops1:<input type="text" name="stops"><br>  
       vehicles: <input type="text" name="vehicles"><br>
+      vehicles1: <input type="text" name="vehicles"><br>
       commons: <input type="text" name="commons"><br>
        
       <input type="submit" value="submit">         
