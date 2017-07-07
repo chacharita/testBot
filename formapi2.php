529a88dea1d1d11ed7b9b783e5ea0fd3aecae53e
chacharita <chaz9008@gmail.com>
2017-07-07 10:18:06 +0700

    Create formapi2.php

---
+++ formapi2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div>
+    
+   <h1>REGISTER</h1>
+   
+    <form action="api2.php" method="GET"> 
+
+      date:<input type="text" name="date_time"><br>  
+      polyline: <input type="text" name="polyline"><br>
+      service_time : <input type="text" name="service_time"><br>
+      mode: <input type="text" name="mode"><br>  
+      toll: <input type="text" name="toll"><br><br>    
+      vehicle: <input type="text" name="vehicle"><br> 
+      <input type="submit" value="submit">         
+    </form>
+    </div>
+</body>
+</html>
