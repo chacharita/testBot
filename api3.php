322fb53c76d4c28333fc308101dd8afda9309d9a
chacharita <chaz9008@gmail.com>
2017-07-07 12:41:33 +0700

    Update api3.php

--- api3.php
+++ api3.php
@@ -9,7 +9,7 @@ $vehicles = $_POST['vehicles'];
 $vehicles1 = $_POST['vehicles'];
 $commons = $_POST['commons'];
 
-//var_dump($_POST);
+var_dump($_POST);
     
     $header = array(
             'Content-Type:application/x-www-form-urlencoded',
@@ -28,4 +28,5 @@ $commons = $_POST['commons'];
             curl_close($ch);
     
     var_dump($result);
+echo "dd";
 ?>
