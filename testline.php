7d5524d7ac23abe6debdf26645b647381b6d1f2a
chacharita <chaz9008@gmail.com>
2017-07-12 14:47:43 +0700

    Update testline.php

--- testline.php
+++ testline.php
@@ -15,7 +15,6 @@ $proxyauth = 'http://fixie:bBt21X0wwYroR2Z@velodrome.usefixie.com:80';
             "to"        => 'U16c42e452723cf9c2682d7bf0001b0d8',
             "messages"  => [$messages]
         );
-      var_dump($post_data);exit();
         //send_line_msg($post_data, $strAccessToken);
         $access_token = 'xjGIR1MZNjzmCI9qagfTX7ksvvmLJYmOZZfCaAvY52kld2Hm4SeDJtzRv31ZDyIum31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDtKyONakxR1kk6ADFzu3Ko5WWqxfhMcufHu3ldcWEhREAdB04t89/1O/w1cDnyilFU=';
         //$access_token = 'f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=';
@@ -31,8 +30,8 @@ $proxyauth = 'http://fixie:bBt21X0wwYroR2Z@velodrome.usefixie.com:80';
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
  
-        $result = curl_exec($ch);
+        $result = curl_exec($ch);return $result;
         curl_close($ch);
-        return $result;
+        
  
  ?>
