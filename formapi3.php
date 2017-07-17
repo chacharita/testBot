82b1519cc5bff046020b46dfbe7a08e11ca30b54
chacharita <chaz9008@gmail.com>
2017-07-17 20:18:19 +0700

    Update formapi3.php

--- formapi3.php
+++ formapi3.php
@@ -7,11 +7,9 @@
     
     <form action="api3.php" method="POST"> 
 
-      stops____: <input type="text" name="stops" value="1,13.8036917,100.5542416,30,08:00:00,17:00:00" size="20"><br> 
-      stops1___: <input type="text" name="stops" value="n2,13.8755595,100.6191486,30,08:00:00,17:00:00" size="19"><br> 
-      vehicles_: <input type="text" name="vehicles" value="1,13.8036917,100.5542416,13.8036917,100.5542416,08:00:00,17:00:00,80" size="19"><br>
-      vehicles1:<input type="text" name="vehicles"  value="2,13.79518,100.61217,13.81042,100.65585,08:00:00,17:00:00,40" size="19"><br>
-      commons__: <input type="text" name="commons" value="2017-06-01,1,0,1" size="19"><br>
+      stops____: <input type="text" name="stops" value="1,13.8036917,100.5542416,30,08:00:00,17:00:00\n2,13.8755595,100.6191486,30,08:00:00,17:00:00" size="20"><br> 
+      vehicles_: <input type="text" name="vehicles" value="1,13.8036917,100.5542416,13.8036917,100.5542416,08:00:00,17:00:00,80\n2,13.79518,100.61217,13.81042,100.65585,08:00:00,17:00:00,40"><br>
+      commons__: <input type="text" name="commons" value="2017-06-01,1,0,1"><br>
        
       <input type="submit" value="submit">         
     </form>
