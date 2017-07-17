8d23d35366eab3716b6bebfa39c4b93ed6b1dcfc
chacharita <chaz9008@gmail.com>
2017-07-17 20:05:56 +0700

    Update api2.php

--- api2.php
+++ api2.php
@@ -2,7 +2,7 @@
 
 <?php
    
-$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/smvrp";     
+$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/smvrp?date_time=2016-06-14T08:00:00&polyline=%7BjesAayaeR_lHbgAvlEcxI%7EtFou%40wwGpoO&service_time=30&mode=1&toll=1&vehicle=2";     
 $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 
 
