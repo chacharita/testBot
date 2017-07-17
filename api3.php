b0f14245eb39df312dfc45fcef6a087755a3654a
chacharita <chaz9008@gmail.com>
2017-07-17 23:14:05 +0700

    Update api3.php

--- api3.php
+++ api3.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div>
+    
+   <h1>Multi-Drop/Multi-Vehicle Routing</h1>
+    
+    <form method="POST"> 
+
+      stops____: <input type="text" value = "1,13.8036917,100.5542416,30,08:00:00,17:00:00\n2,13.8755595,100.6191486,30,08:00:00,17:00:00" name="stops"><br> 
+      vehicles_: <input type="text" value = "1,13.8036917,100.5542416,13.8036917,100.5542416,08:00:00,17:00:00,80\n2,13.79518,100.61217,13.81042,100.65585,08:00:00,17:00:00,40" name="vehicles"><br>
+      commons__: <input type="text" value = "2017-06-01,1,0,1"	name="commons"><br>
+       
+      <input type="submit" value="submit">         
+    </form>
+    </div>
 <?php
 $stops =    $_POST['stops'];
 $vehicles = $_POST['vehicles'];
@@ -22,4 +38,8 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 var_dump($result);
+  var_dump($_POST);
+?>
+</body>
+</html>
 
