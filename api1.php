66726d9806181a7ca0bfd6d3490193e52e1fd43f
chacharita <chaz9008@gmail.com>
2017-07-17 18:27:58 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -2,7 +2,7 @@
 
 <?php
  echo 21;  
-$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting";     
+$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-04-26T08:00:00&st_y=13.803691&st_x=100.554241&en_y=13.8755595&en_x=100.6191486&toll=0";     
 $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 
 
@@ -29,7 +29,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
             curl_setopt($ch, CURLOPT_GET, true);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS,($_GET));
+            //curl_setopt($ch, CURLOPT_POSTFIELDS,($_GET));
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
             $result = curl_exec($ch);
             curl_close($ch);
