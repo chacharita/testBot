5185379cec9d0bc2eb5cc7681c52b9f93e53b26a
chacharita <chaz9008@gmail.com>
2017-07-17 21:26:50 +0700

    Update api3.php

--- api3.php
+++ api3.php
@@ -6,8 +6,6 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 $stops = $_POST['stops'];
 $vehicles = $_POST['vehicles'];
 $commons = $_POST['commons'];
-
-//var_dump($_POST);
     
     $header = array(
             'Content-Type:application/x-www-form-urlencoded',
@@ -25,6 +23,6 @@ $commons = $_POST['commons'];
             $result = curl_exec($ch);
             curl_close($ch);
     
-    var_dump(json_decode($result));
+    var_dump($result);
 
 ?>
