df45a62064e327a8c11585b7a4e2dc65b6c4e036
chacharita <chaz9008@gmail.com>
2017-07-17 22:20:40 +0700

    Update api3.php

--- api3.php
+++ api3.php
@@ -1,31 +1,33 @@
 <?php
-echo "55"; 
-
-$stops = $_POST['stops'];
+$stops =    $_POST['stops'];
 $vehicles = $_POST['vehicles'];
-$commons = $_POST['commons'];
-var_dump($_POST); 
+$commons =  $_POST['commons'];
 
-$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/mvrp";     
-$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
+$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/mvrp";
+$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";	
 
-    $header = array(
-            'Content-Type:application/x-www-form-urlencoded',
-            'Authorization: Bearer'. $strAccessToken
-            );
-   
-    
-    $ch = curl_init($url);
- 
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST,'POST');
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);        
-            curl_setopt($ch, CURLOPT_POSTFIELDS($_POST));
-            curl_setopt($ch, CURLOPT_HTTPHEADER,$header);
-            $result = curl_exec($ch);
-            curl_close($ch);
-    //return $result;
-    var_dump($result);
+$header = array(
+'Content-Type:application/x-www-form-urlencoded',
+'Authorization: Bearer ' . $strAccessToken
+);
 
-?>
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+$result = curl_exec($ch);
+curl_close($ch);
+var_dump($result)."<br><br>";
+echo "--------------------\n";
+var_dump($stops)."<br><br>";
+echo "--------------------\n";
+var_dump($vehicles)."<br><br>";
+echo "--------------------\n";
+var_dump($commons)."<br><br>";
+echo "--------------------\n";
+var_dump($_POST)."<br><br>";
+echo "--------------------\n";
+var_dump($ch)."<br><br>";
