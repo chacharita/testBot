0bb9432a097c463a594b6f9575f18f064cc30a2c
chacharita <chaz9008@gmail.com>
2017-07-17 22:14:58 +0700

    Update api3.php

--- api3.php
+++ api3.php
@@ -1,12 +1,13 @@
 <?php
-echo "55";  
-$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/mvrp";     
-$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
+echo "55"; 
 
 $stops = $_POST['stops'];
 $vehicles = $_POST['vehicles'];
 $commons = $_POST['commons'];
 var_dump($_POST); 
+
+$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/mvrp";     
+$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 
     $header = array(
             'Content-Type:application/x-www-form-urlencoded',
