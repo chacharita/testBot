45d75a338da8417c69ede293bc1162af13cd0e64
chacharita <chaz9008@gmail.com>
2017-07-17 22:00:42 +0700

    Update api3.php

--- api3.php
+++ api3.php
@@ -6,7 +6,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 $stops = $_POST['stops'];
 $vehicles = $_POST['vehicles'];
 $commons = $_POST['commons'];
-    
+var_dump($_POST);    
     $header = array(
             'Content-Type:application/x-www-form-urlencoded',
             'Authorization: Bearer'. $strAccessToken
