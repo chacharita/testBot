229077dc33f5bbd29d727e63fecb0e294d247f8f
chacharita <chaz9008@gmail.com>
2017-07-18 11:18:57 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -36,7 +36,10 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
        
    
     //echo "response";
+    //echo json_dec 
+    var_dump(json_encode($result));
     var_dump(json_decode($result));
+    
 
 
 ?>
