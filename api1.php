ca221b4e17cc606566bb9fe4deee1afc60ca9d30
chacharita <chaz9008@gmail.com>
2017-07-18 14:09:42 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -29,7 +29,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
        // echo "                                 ";
         $da = rawurldecode($de);
         $aa = json_decode($da);
-        $bb = aa[0]->Route;
+        $bb = $aa[0]->Route;
 //         $cc = rawurlencode($bb);
 // var_dump($aa);
        var_dump($bb);
