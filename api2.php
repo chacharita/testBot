d25ba027c900a250af46cbc0eaac72a15c6bebbd
chacharita <chaz9008@gmail.com>
2017-07-18 11:41:04 +0700

    Update api2.php

--- api2.php
+++ api2.php
@@ -1,5 +1,3 @@
-
-
 <?php
    
 $url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/smvrp?date_time=2016-06-14T08:00:00&polyline=%7BjesAayaeR_lHbgAvlEcxI%7EtFou%40wwGpoO&service_time=30&mode=1&toll=1&vehicle=2";     
@@ -17,7 +15,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
             curl_setopt($ch, CURLOPT_GET, true);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS,(0));
+            curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode(0));
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
             $result = curl_exec($ch);
             curl_close($ch);
