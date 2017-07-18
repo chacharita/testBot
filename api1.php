74de9dc943cee994d4dea10d7fbe0fbab627342f
chacharita <chaz9008@gmail.com>
2017-07-18 16:58:58 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -1,17 +1,12 @@
-
-
 <?php
-
-$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-04-26T08:00:00&st_y=13.8036917
-&st_x=100.5542416&en_y=13.8755595&en_x=100.6191486&toll=1"; 
-
-$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
+$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-04-26T08:00:00&st_y=13.8036917&st_x=100.5542416&en_y=13.8755595&en_x=100.6191486&toll=1";		
+$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";		
 
     $header = array(
             'Content-Type: application/json',
             'Authorization: Bearer ' . $strAccessToken
             );
-    
+   
     
     $ch = curl_init($url);
  
@@ -22,8 +17,13 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
             $result = curl_exec($ch);
             curl_close($ch);
        
+   
+    
+    var_dump(json_decode($result));
+    
+    
+?>
 
-            var_dump(json_decode($result));
 // // var_dump(urldecode($result));
 // $a = json_decode($result);
 // // echo 'this is decode\n';
