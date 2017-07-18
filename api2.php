de3ceb087dc4593b8c03580577a90085d09f0e27
chacharita <chaz9008@gmail.com>
2017-07-18 12:40:35 +0700

    Update api2.php

--- api2.php
+++ api2.php
@@ -15,16 +15,10 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
             curl_setopt($ch, CURLOPT_GET, true);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            //curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($url));
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
             $result = curl_exec($ch);
             curl_close($ch);
        
-   var_dump(json_decose($result));
-    //echo "response";
-    //echo urldecode.$result;
-      //echo utf8_decode(urldecode("date_time=2016-06-14T08:00:00&polyline=%7BjesAayaeR_lHbgAvlEcxI%7EtFou%40wwGpoO&service_time=30&mode=1&toll=1&vehicle=2"));
-     // echo urldecode($result);
-        //echo urldecode("?date_time=2016-06-14T08:00:00&polyline=%7BjesAayaeR_lHbgAvlEcxI%7EtFou%40wwGpoO&service_time=30&mode=1&toll=1&vehicle=2");
-
+   var_dump(json_decode($result));
+    
 ?>
