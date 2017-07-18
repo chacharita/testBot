f21f5a6f91f98b4e8b742e0393e764e908811881
chacharita <chaz9008@gmail.com>
2017-07-18 14:03:59 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -25,10 +25,13 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
   $de = rawurlencode($result);
      //$de =json_decode($result);
         
-        var_dump($de);
-        echo "                                 ";
+       // var_dump($de);
+       // echo "                                 ";
         $da = rawurldecode($de);
-        var_dump(json_decode($da));
+        $aa = json_decode($da);
+        $bb = aa->route;
+        $cc = rawurldecode($bb);
+        var_dump($cc);
 
 
 ?>
