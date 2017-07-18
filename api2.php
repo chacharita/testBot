fb0c74ebaddbb1d30741bdc052498dd851982e99
chacharita <chaz9008@gmail.com>
2017-07-18 12:11:32 +0700

    Update api2.php

--- api2.php
+++ api2.php
@@ -23,8 +23,8 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
    
     //echo "response";
     //echo urldecode.$result;
-      echo utf8_decode(urldecode("date_time=2016-06-14T08:00:00&polyline=%7BjesAayaeR_lHbgAvlEcxI%7EtFou%40wwGpoO&service_time=30&mode=1&toll=1&vehicle=2"));
-      echo urldecode($result);
-
+      //echo utf8_decode(urldecode("date_time=2016-06-14T08:00:00&polyline=%7BjesAayaeR_lHbgAvlEcxI%7EtFou%40wwGpoO&service_time=30&mode=1&toll=1&vehicle=2"));
+     // echo urldecode($result);
+        echo urldecode("?date_time=2016-06-14T08:00:00&polyline=%7BjesAayaeR_lHbgAvlEcxI%7EtFou%40wwGpoO&service_time=30&mode=1&toll=1&vehicle=2");
 
 ?>
