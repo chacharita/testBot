e698f873382e76fc1d528482544431696886e2eb
chacharita <chaz9008@gmail.com>
2017-07-18 12:32:49 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -1,7 +1,7 @@
 
 
 <?php
- echo 22;  
+
 $url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-04-26T08:00:00&st_y=13.803691&st_x=100.554241&en_y=13.8755595&en_x=100.6191486&toll=0";     
 $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 
