a9c1ed0ecdbf7c98692226cb2b308003b58833b2
chacharita <chaz9008@gmail.com>
2017-07-18 11:23:12 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -5,19 +5,6 @@
 $url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-04-26T08:00:00&st_y=13.803691&st_x=100.554241&en_y=13.8755595&en_x=100.6191486&toll=0";     
 $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 
-
-  // var_dump($_GET);
- 
-//    $post_data =array(  
-//         $date_time='yyyy-mm-dd' ,
-//         $st_y = '08:00:00',
-//         $st_x= '08:00:00',     
-//         $en_y= '17:00:00' ,      
-//         $en_x= '17:00:00'  ,    
-//         $toll= '[0 or 1]'
-//     ); 
-       
-    
     $header = array(
             'Content-Type: application/json',
             'Authorization: Bearer ' . $strAccessToken
@@ -29,6 +16,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
             curl_setopt($ch, CURLOPT_GET, true);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($url));
             //curl_setopt($ch, CURLOPT_POSTFIELDS,($_GET));
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
             $result = curl_exec($ch);
@@ -37,7 +25,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
    
     //echo "response";
     //echo json_dec 
-    var_dump(json_encode($result));
+    //var_dump(json_encode($result));
     var_dump(json_decode($result));
     
 
