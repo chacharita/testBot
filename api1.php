3ab29ee1769f754e1711f08f98b9abe7e3360f44
chacharita <chaz9008@gmail.com>
2017-07-18 14:13:05 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -32,7 +32,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
         $bb = $aa[0]->Legs;
 //         $cc = rawurlencode($bb);
 var_dump($aa);
-//        var_dump($bb);
+       var_dump($bb);
 
 
 ?>
