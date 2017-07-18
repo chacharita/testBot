0cd21db4b620b97663680e19a5bf517ea957c5fc
chacharita <chaz9008@gmail.com>
2017-07-18 12:01:48 +0700

    Update api2.php

--- api2.php
+++ api2.php
@@ -15,7 +15,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
             curl_setopt($ch, CURLOPT_GET, true);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode(0));
+            curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($url));
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
             $result = curl_exec($ch);
             curl_close($ch);
