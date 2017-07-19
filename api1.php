5d5d535deb28ede327ca227303bc2596fe685bfe
chacharita <chaz9008@gmail.com>
2017-07-19 10:11:26 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -19,15 +19,15 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
        
    
     
-    var_dump(json_decode($result));
+    //var_dump(json_decode($result));
     
 
-// // var_dump(urldecode($result));
-// $a = json_decode($result);
-// // echo 'this is decode\n';
-// var_dump($a[0]->Legs[0]->Route);
-// // echo 'this is urldecode\n';
-// // var_dump($a);
+    var_dump(urldecode($result));
+    $a = json_decode($result);
+    echo 'this is decode\n';
+    var_dump($a[0]->Legs[0]->Route);
+     echo 'this is urldecode\n';
+     var_dump($a);
 
    
 //       //  $de = rawurlencode($result);
