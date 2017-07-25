5df72fe90d761b51c818b6eff23aef4f6e090eb4
chacharita <chaz9008@gmail.com>
2017-07-25 10:17:03 +0700

    Update select_master.php

--- select_master.php
+++ select_master.php
@@ -95,8 +95,18 @@
                                             
                                                 $de_member = json_decode($result);
                                                 $count = count($de);
+                                                
+                                                echo $show->id,$show->name,$show->token;
                                                
                                             ?>
+                                            <?php 
+                                            
+                                            if($de_line_mas->id == $de_member->line_master_id){
+                                                    for($i=0;$i<$count;$i++){ ?>
+                                                        <div class="checkbox">
+                                                            <label><input type="checkbox" value="<?php echo $de_member[$i]->user_id; ?>" name="mid[]"> <?php echo $de_member[$i]->member_name; echo "  "; echo $de_member[$i]->user_id; ?></label>
+                                                        </div>
+                                           <?php }}?>
                                           
                                           
                                         </div>
