07fc28bf2dcd40cf2b0bc21517dfcc40580f5501
chacharita <chaz9008@gmail.com>
2017-07-25 13:14:21 +0700

    Update testmember.php

--- testmember.php
+++ testmember.php
@@ -100,7 +100,7 @@
                                                 ?>
                                             
                                             <?php   
-                                                    $line_a_id = $_POST['token_line_mas'];
+                                                    $line_a_id = $_GET['token_line_mas'];
                                                     var_dump($line_a_id);
                                                     if($de_line_mas->id == $de->line_master_id){
 //                                                     var_dump ($de_line_mas);
