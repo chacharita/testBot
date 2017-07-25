2004eb58fa760a400fe8c3fb1d04dae913ecc353
chacharita <chaz9008@gmail.com>
2017-07-25 12:17:21 +0700

    Update testmember.php

--- testmember.php
+++ testmember.php
@@ -96,16 +96,15 @@
                                                 $de = json_decode($result);
                                                 $count = count($de);
                                                
+                                                
                                                 ?>
-                                           
                                             
-                                            <?php
-                                                                            
+                                            <?php if($de_line_mas->id == $de->line_master_id){
                                                     for($i=0;$i<$count;$i++){ ?>
                                                         <div class="checkbox">
-                                                       <label><input type="checkbox" value="<?php echo $de[$i]->user_id; ?>" name="mid[]"> <?php echo $de[$i]->member_name; echo "  "; echo $de[$i]->user_id; ?></label>
+                                                            <label><input type="checkbox" value="<?php echo $de[$i]->user_id; ?>" name="mid[]"> <?php echo $de[$i]->member_name; echo "  "; echo $de[$i]->user_id; ?></label>
                                                         </div>
-                                           <?php }?>
+                                           <?php }}?>
                                         </div>
                                     </div>
                                     <div class="modal-footer">
