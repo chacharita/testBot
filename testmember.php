0a591784310cc0f5b729dcab6da66ad53cad6273
chacharita <chaz9008@gmail.com>
2017-07-25 12:36:52 +0700

    Update testmember.php

--- testmember.php
+++ testmember.php
@@ -100,7 +100,8 @@
                                                 ?>
                                             
                                             <?php if($de_line_mas->id == $de->line_master_id){
-                                                    var_dump ($da_line_mas->id);
+                                                    var_dump ($de_line_mas->id);
+                                                    var_dump ($de->line_master_id);
                 
                                                     for($i=0;$i<$count;$i++){ ?>
                                                         <div class="checkbox">
