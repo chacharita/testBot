24ab5d80a7169eab92be27c7631b7a481ad30f22
chacharita <chaz9008@gmail.com>
2017-07-25 11:37:44 +0700

    Update testmember.php

--- testmember.php
+++ testmember.php
@@ -101,9 +101,9 @@
                                             <?php 
                                             
                                             if($de_line_mas->id == $de->line_master_id){
-                                                    for($i=0;$i<$count;$i++){ ?>
+                                                    //for($i=0;$i<$count;$i++){ ?>
                                                         <div class="checkbox">
-                                                            <label><input type="checkbox" value="<?php echo $de[$i]->user_id; ?>" name="mid[]"> <?php echo $de[$i]->member_name; echo "  "; echo $de[$i]->user_id; ?></label>
+                                                            <label><input type="checkbox" value="<?php echo $de->user_id; ?>" name="mid"> <?php echo $de->member_name; echo "  "; echo $de->user_id; ?></label>
                                                         </div>
                                            <?php }}?>
                                         </div>
