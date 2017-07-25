08ab985ef575a597a48c9acbd08537894a13f2f2
chacharita <chaz9008@gmail.com>
2017-07-25 14:38:16 +0700

    Update testmember.php

--- testmember.php
+++ testmember.php
@@ -64,7 +64,7 @@
                     
                     <!--buttonMember-->
                     <div class="form-group" align="center">
-                        <button type="button" onclick="member_bot" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" style="margin-top:30px;margin-bottom:20px;">
+                        <button type="button"  class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" style="margin-top:30px;margin-bottom:20px;">
                         MEMBER
                         </button>
                     </div>
