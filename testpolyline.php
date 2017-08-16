7bbfd6c63dc338fab76866176cb1736fe29e3f1f
chacharita <chaz9008@gmail.com>
2017-08-16 16:42:49 +0700

    Update testpolyline.php

--- testpolyline.php
+++ testpolyline.php
@@ -4,7 +4,7 @@
 # https://developers.google.com/maps/documentation/utilities/polylinealgorithm
 # in reverse order and inverted (i.e. left shift -> right shift, add -> subtract)
 
-$string = "udymAgovfR_opAi`]cwmAxy~@eu\zzV";
+$string = "c|xrAss}dRqtpCpm{B~wSluhEjhvDidPh{~I{v_C~iwAftcApe_DpqnBlzlB}nr@~gqB?jsyDftcA{ee@yfsDfexAhxv@h`tA{nyCvmzBquaBy{rg@idPixfEiivIojd@wn`GgpdHnabDsz`Fren@bdp@yb`Ejf_DghjBbil@pytA?lybHqvkDj}uG_ovCbpwDawc@zjfDbytA_{KfkjCi`]bnyAd|pC";
 # Step 11) unpack the string as unsigned char 'C'
 $byte_array = array_merge(unpack('C*', $string));
 $results = array();
