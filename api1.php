e1e35c514d55ffa3322c65a751cfdc10a89063ce
chacharita <chaz9008@gmail.com>
2017-08-18 12:11:19 +0700

    Update api1.php

--- api1.php
+++ api1.php
@@ -1,6 +1,5 @@
 <?php
-$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-07-01T10:08:00
-&st_y=13.795082&st_x=100.596549&en_y=13.107056&en_x=101.133106&toll=1";		
+$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-07-01T10:08:00&st_y=13.795082&st_x=100.596549&en_y=13.107056&en_x=101.133106&toll=1";		
 $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";		
 
     $header = array(
@@ -20,7 +19,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
        
    
     
-    var_dump(json_decode($result));
+    var_dump($result);
     
 
    // var_dump(urldecode($result));
