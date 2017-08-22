55cab5945f351b4d2309f5b79a8229af7df8110a
chacharita <chaz9008@gmail.com>
2017-08-22 16:18:38 +0700

    Create formpoly.php

---
+++ formpoly.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div>
+    
+   <h1>REGISTER</h1>
+   
+    <form action="polyline.php" method="POST"> 
+      INPUT POLYLINE :
+      <input type = "text" name="polyline"><br>
+      
+      <input type="submit" value="submit">         
+    </form>
+    </div>
+</body>
+</html>
