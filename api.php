163fe7edd9abeb141da1c2d47eb1a336bba2e340
chacharita <chaz9008@gmail.com>
2017-08-29 13:59:53 +0700

    Update api.php

--- api.php
+++ api.php
@@ -1,22 +1,29 @@
 <?php
-$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting?date_time=2017-07-01T10:08:00&st_y=13.795082&st_x=100.596549&en_y=13.107056&en_x=101.133106&toll=1";		
-$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";		
-    $header = array(
-            'Content-Type: application/json',
-            'Authorization: Bearer ' . $strAccessToken
-            );
-   
-    
-    $ch = curl_init($url);
- 
-            curl_setopt($ch, CURLOPT_GET, true);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-            $result = curl_exec($ch);
-            curl_close($ch);
-       
-   
-    
-    var_dump($result);
-    ?>
+$request = new HttpRequest();
+$request->setUrl('https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting');
+$request->setMethod(HTTP_METH_GET);
+
+$request->setQueryData(array(
+  'date_time' => '2017-04-26T08:00:00',
+  'st_y' => '13.8036917',
+  'st_x' => '100.5542416',
+  'en_y' => '13.8755595',
+  'en_x' => '100.6191486',
+  'toll' => '1'
+));
+
+$request->setHeaders(array(
+  'postman-token' => 'ff7180e0-da3c-e904-c197-4b5a1fff509a',
+  'cache-control' => 'no-cache',
+  'content-type' => 'application/json',
+  'authorization' => 'Bearer 31cc9ed0-50f1-3b99-b4d1-8aca20de6213'
+));
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
+}
+?>
