176c6636d6c1f2539925d62204c0947ab9cb4504
chacharita <chaz9008@gmail.com>
2017-09-04 23:04:12 +0700

    Update poly.php

--- poly.php
+++ poly.php
@@ -1,24 +1,20 @@
 <?php
-$address = "Kathmandu, Nepal";
-$url = "http://maps.google.com/maps/api/geocode/json?address=".urlencode($address);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);    
-$responseJson = curl_exec($ch);
-curl_close($ch);
-
-$response = json_decode($responseJson);
-
-if ($response->status == 'OK') {
-    $latitude = $response->results[0]->geometry->location->lat;
-    $longitude = $response->results[0]->geometry->location->lng;
-
-    echo 'Latitude: ' . $latitude;
-    echo '<br />';
-    echo 'Longitude: ' . $longitude;
-} else {
-    echo $response->status;
-    var_dump($response);
-}    
+ 
+/**
+ * Issue #10
+ * Wrong rounding method for google.
+ * @link https://github.com/emcconville/google-map-polyline-encoding-tool/issues/10
+ */
+class Issue10 extends PHPUnit_Framework_TestCase
+{
+    public function testRounding()
+    {
+        $originalPoints = array(48.000006, 2.000004, 48.00001, 2.0);
+        $encoded = Polyline::Encode($originalPoints);
+        $this->assertEquals('a_~cH_seK??', $encoded);
+        $decodedPoints = Polyline::Decode($encoded);
+        $this->assertTrue($decodedPoints[0] === $decodedPoints[2]);
+        $this->assertTrue($decodedPoints[1] === $decodedPoints[3]);
+    }
+}
 ?>
