b67b4237d5ddde38acfd5b1787d1f494c82308f4
chacharita <chaz9008@gmail.com>
2017-09-04 22:00:27 +0700

    Create poly.php

---
+++ poly.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../PolylineEncoder.php';
+$stringToDecode = "_p~iF~ps|U_ulLnnqC_mqNvxq`@";
+echo "String to decode :\t$stringToDecode\n";
+$decodedPoints = PolylineEncoder::decodeValue($stringToDecode);
+print_r($decodedPoints);
+?>
