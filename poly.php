bb7809332f5d252e15f31afa8206b95747e3d90a
chacharita <chaz9008@gmail.com>
2017-09-04 22:41:53 +0700

    Update poly.php

--- poly.php
+++ poly.php
@@ -1,7 +1,30 @@
-<?php
-//require_once '../PolylineEncoder.php';
-$stringToDecode = "_p~iF~ps|U_ulLnnqC_mqNvxq`@";
-echo "String to decode :\t$stringToDecode\n";
-$decodedPoints = PolylineEncoder::decodeValue($stringToDecode);
-print_r($decodedPoints);
-?>
+<?php    
+function decodeValue($value) {
+        $index = 0;
+        $points = array();
+        $lat = 0;
+        $lng = 0;
+        while ($index < strlen($value)) {
+            $b;
+            $shift = 0;
+            $result = 0;
+            do {
+                $b = ord(substr($value, $index++, 1)) - 63;
+                $result |= ($b & 0x1f) << $shift;
+                $shift += 5;
+            } while ($b > 31);
+            $dlat = (($result & 1) ? ~($result >> 1) : ($result >> 1));
+            $lat += $dlat;
+            $shift = 0;
+            $result = 0;
+            do {
+                $b = ord(substr($value, $index++, 1)) - 63;
+                $result |= ($b & 0x1f) << $shift;
+                $shift += 5;
+            } while ($b > 31);
+            $dlng = (($result & 1) ? ~($result >> 1) : ($result >> 1));
+            $lng += $dlng;
+            $points[] = array('x' => $lat/100000, 'y' => $lng/100000);
+        }
+        return $points;
+    }
