926ff80a1b09efe7d0d94840cd51c70db2500a88
chacharita <chaz9008@gmail.com>
2017-09-04 22:01:27 +0700

    Update poly.php

--- poly.php
+++ poly.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../PolylineEncoder.php';
+//require_once '../PolylineEncoder.php';
 $stringToDecode = "_p~iF~ps|U_ulLnnqC_mqNvxq`@";
 echo "String to decode :\t$stringToDecode\n";
 $decodedPoints = PolylineEncoder::decodeValue($stringToDecode);
