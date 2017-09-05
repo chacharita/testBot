e2fbf375b9d797e7d7db2bebf78e3b04056e96d3
chacharita <chaz9008@gmail.com>
2017-09-05 13:49:46 +0700

    Update poly1.php

--- poly1.php
+++ poly1.php
@@ -1,30 +1,28 @@
 <?php
 
-$lat = "13.05037";
-$lng = "100.93107";
-$data = file_get_contents("http://maps.google.com/maps/api/geocode/json?latlng=$lat,$lng&sensor=false");
-$data = json_decode($data);
-$add_array  = $data->results;
-$add_array = $add_array[0];
-$add_array = $add_array->address_components;
-//$country = "Not found";
-$state = "Not found"; 
-$city = "Not found";
-//$place = "Not found";
-//$street = "Not found";
-foreach ($add_array as $key) {
-  if($key->types[0] == 'administrative_area_level_2')
-  {
-    $city = $key->long_name;
-  }
-  if($key->types[0] == 'administrative_area_level_1')
-  {
-    $state = $key->long_name;
-  }
-  if($key->types[0] == 'country')
-  {
-     $country = $key->long_name;
-  }
-}
-echo "State : ".$state." ,City : ".$city.";
+$lat = "13.60845";
+      $lng = "100.54164";
+      $data = file_get_contents("http://maps.google.com/maps/api/geocode/json?latlng=$lat,$lng&sensor=false");
+      $data = json_decode($data);
+      $add_array  = $data->results;
+      $add_array = $add_array[0];
+      $add_array = $add_array->address_components;
+      $country = "Not found";
+      $state = "Not found"; 
+      $city = "Not found";
+      foreach ($add_array as $key) {
+        if($key->types[0] == 'administrative_area_level_2')
+        {
+          $city = $key->long_name;
+        }
+        if($key->types[0] == 'administrative_area_level_1')
+        {
+          $state = $key->long_name;
+        }
+        if($key->types[0] == 'country')
+        {
+          $country = $key->long_name;
+        }
+      }
+      echo "Country : ".$country." ,State : ".$state." ,City : ".$city;
 ?>
