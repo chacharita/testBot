1a10ae4f05466cf42585d887493f97c903c7772d
chacharita <chaz9008@gmail.com>
2017-09-05 11:58:22 +0700

    Update poly1.php

--- poly1.php
+++ poly1.php
@@ -1,21 +1,22 @@
 <?php
 
-// $address = "India+Panchkula";
-// $url = "http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=India";
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL, $url);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-// $response = curl_exec($ch);
-// curl_close($ch);
-// $response_a = json_decode($response);
-// echo $lat = $response_a->results[0]->geometry->location->lat;
-// echo "<br />";
-// echo $long = $response_a->results[0]->geometry->location->lng;
-function get_name_locations() {
-  $address = document.getElementById("13.73621,100.65039").value;
+$address = "India+Panchkula";
+$url = "http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=India";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$response = curl_exec($ch);
+curl_close($ch);
+$response_a = json_decode($response);
+echo $lat = $response_a->results[0]->geometry->location->lat;
+echo "<br />";
+echo $long = $response_a->results[0]->geometry->location->lng;
+
+function get_name_locations($response_a=null) {
+  $address = document.getElementById("$response_a").value;
   $geocoder = new google.maps.Geocoder();
   $a = geocoder.geocode({address: address}, 
   function(results, status) {
