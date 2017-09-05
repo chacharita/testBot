12bac2c9545d63fafba436062f57d6b88a481aa3
chacharita <chaz9008@gmail.com>
2017-09-05 12:45:27 +0700

    Create poly_name.php

---
+++ poly_name.php
@@ -0,0 +1,25 @@
+<?php
+
+function getAddress($latitude,$longitude){
+$latitude = '38.897952';
+$longitude = '-77.036562';
+$address = getAddress($latitude,$longitude);
+$address = $address?$address:'Not found';
+    if(!empty($latitude) && !empty($longitude)){
+        //Send request and receive json data by address
+        $geocodeFromLatLong = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.trim($longitude).'&sensor=false'); 
+        $output = json_decode($geocodeFromLatLong);
+        $status = $output->status;
+        //Get address from json data
+        $address = ($status=="OK")?$output->results[1]->formatted_address:'';
+        //Return address of the given latitude and longitude
+        if(!empty($address)){
+            return $address;
+        }else{
+            return false;
+        }
+    }else{
+        return false;   
+    }
+}
+?>
