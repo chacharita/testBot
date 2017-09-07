e6c8da19b9f48844786738b334b2d328deedd0a4
chacharita <chaz9008@gmail.com>
2017-09-07 16:08:57 +0700

    Create curlget_lo.php

---
+++ curlget_lo.php
@@ -0,0 +1,43 @@
+<?php
+function get_lat_long($address){
+ 
+sleep(5);
+ 
+$address = str_replace(" ", "+", $address);
+ 
+$region = 'US';
+ 
+$url =    "http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=$region";
+ 
+$ch = curl_init();
+ 
+curl_setopt($ch, CURLOPT_URL, $url);
+ 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+ 
+curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+ 
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+ 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+ 
+$response = curl_exec($ch);
+ 
+curl_close($ch);
+ 
+$response_a = json_decode($response);
+ 
+$lat = $response_a->results[0]->geometry->location->lat;
+ 
+$long = $response_a->results[0]->geometry->location->lng;
+ 
+$latlon = array($lat, $long);
+ 
+var_dump($response_a);
+ 
+var_dump($latlon);
+ 
+return $latlon;
+ 
+}
+?>
