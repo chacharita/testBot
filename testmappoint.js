20ae513f8ba0a734edaefe660b43b0bbc5cba427
chacharita <chaz9008@gmail.com>
2017-09-22 11:48:27 +0700

    Update testmappoint.js

--- testmappoint.js
+++ testmappoint.js
@@ -1,55 +1,17 @@
+var map = new GMap2(document.getElementById("map-canvas"));
+  map.addControl(new GLargeMapControl());
+  map.addControl(new GMapTypeControl());
+  map.setCenter(new GLatLng(<?=$lat;?>,<?=$lng;?>), 6);
 
-var map;
-
-function renderDirections(result, map) {
-  var directionsRenderer1 = new google.maps.DirectionsRenderer({
-    directions: result,
-    routeIndex: 0,
-    map: map,
-    polylineOptions: {
-      strokeColor: "green"
-    }
-  });
-  console.log("routeindex1 = ", directionsRenderer1.getRouteIndex());
-
-  var directionsRenderer2 = new google.maps.DirectionsRenderer({
-    directions: result,
-    routeIndex: 1,
-    map: map,
-    polylineOptions: {
-      strokeColor: "blue"
-    }
-  });
-  console.log("routeindex2 = ", directionsRenderer2.getRouteIndex()); //line 17
-}
-
-function calculateAndDisplayRoute(origin, destination, directionsService, directionsDisplay, map) {
-  directionsService.route({
-    origin: origin,
-    destination: destination,
-    travelMode: google.maps.TravelMode.DRIVING,
-    provideRouteAlternatives: true
-  }, function(response, status) {
-    if (status === google.maps.DirectionsStatus.OK) {
-      renderDirections(response, map);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-}
-
-function initialize() {
-  var directionsService = new google.maps.DirectionsService();
-  var directionsDisplay = new google.maps.DirectionsRenderer();
-  var map = new google.maps.Map(
-    document.getElementById("map_canvas"), {
-      center: new google.maps.LatLng(37.4419, -122.1419),
-      zoom: 13,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-  directionsDisplay.setMap(map);
-  calculateAndDisplayRoute(new google.maps.LatLng(51.61793418642200, -0.13678550737318), new google.maps.LatLng(51.15788846699750, -0.16364536053269), directionsService, directionsDisplay, map);
+  var point = new GLatLng(<?=$lat;?>,<?=$lng;?>);
+  var marker = createMarker(point,'Welcome:<b></b><br>Second Info Window with an image<br><img src="http://localhost/gps/user_photo/" width=80 height=80>')
+  map.addOverlay(marker);
 
 
-}
-google.maps.event.addDomListener(window, "load", initialize);
+  function createMarker(point,html) {
+        var marker = new GMarker(point);
+        GEvent.addListener(marker, "click", function() {
+          marker.openInfoWindowHtml(html);
+        });
+        return marker;
+      }
