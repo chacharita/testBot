d6194e4931ff41a4b5dffe57ca197cfb36c2706c
chacharita <chaz9008@gmail.com>
2017-10-03 19:26:51 +0700

    Update polyline.php

--- polyline.php
+++ polyline.php
@@ -1,12 +1,10 @@
 <?php
 
-$polyline =    $_POST['polyline'];
-
-$string = "a%60gsA_ovdRii%40%7BWcDcBa%40%5Dm%40SoCmAm%40U%5BUmFyC_BcA%7BGeDcDcBeEsB%7BHqDuDoBeKcFyKsFwSaK%5D%5BcSwJeEcBwY%7BNwJuEwFsCkB%7B%40qDgBuKsFaB_AoDcBcWcM%7BwBefAiE%7BBkD_B_N%7BGeAk%40SAwJ%7BEoCqAs%40QYEi%40Em%40%40m%40G%7B%40U%5DU_%40a%40W_%40Si%40GY%40a%40DSR%5DPM%5CMVEbABr%40Tx%40b%40ZH%5E%40%60Ce%40RGv%40e%40%5C%5DLObAkBjR_c%40wB_AGEQWXa_%40gCea%40c%40gGI%3FuD%7BB%7DUuM";
+$string = "qvbsAuntdRrDoEwe@{LgAhKi@bE_AfFq@jGSnA{@dCk@fAm@|@KR";
 $byte_array = array_merge(unpack('C*', $string));
 $results = array();
 
-$index = 0; # tracks which char in $byte_array
+$index = 0; 
 do {
   $shift = 0;
   $result = 0;
@@ -29,8 +27,5 @@ for ($i = 2; $i < count($results); $i++) {
   $results[$i] += $results[$i - 2];
 }
 var_dump(array_chunk($results,2));
-
-
-
 
 ?>
